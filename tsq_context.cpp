2bfbfa84b70b791b4c3644e9650a9e8fd5d388da
Julien Perrier-cornet <julienperrier241@gmail.com>
2025-10-18 09:03:22 +0200

    Updated copyright notices in accordance with the terms of the MIT license. Re-added aligned memory allocation for the hash table (it was incompatible with the usage of the address sanitizer while debugging).

--- tsq_context.cpp
+++ tsq_context.cpp
@@ -1,20 +1,26 @@
 /*
-** Lib4zip line of sight (los) codec context implementation.
-** Copyright (C) 2024 Julien Perrier-cornet
-**
-** This program is free software: you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Turbosqueeze context implementation.
+ *
+ * Copyright (c) 2024-2025 Julien Perrier-cornet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include <cstdio>
 #include <cstdlib>
@@ -41,7 +47,7 @@
 
 extern "C" void tsqDeallocateContext(struct TSQCompressionContext* ctx)
 {
-    if (ctx->refhash) free(ctx->refhash);
+    if (ctx->refhash) align_free(ctx->refhash);
     free(ctx);
 }
 
@@ -53,7 +59,7 @@ extern "C" struct TSQCompressionContext* tsqAllocateContext()
     if (context)
     {
         context->refhash = nullptr;
-        context->refhash = (uint16_t*) malloc( TSQ_HASH_SZ );
+        context->refhash = (uint16_t*) align_alloc( MAX_CACHE_LINE_SIZE, TSQ_HASH_SZ );
 
         if (!context->refhash)
         {
